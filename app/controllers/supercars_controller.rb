f26fc8edc51ea7b017ceecfb171efdffabe2af7e
Nurbrun <bruno.alfano.rizzo@gmail.com>
2015-03-23 21:12:36 -0400

    made some display changes to the main supercar index..added thumbnail with some display info

--- app/controllers/supercars_controller.rb
+++ app/controllers/supercars_controller.rb
@@ -9,7 +9,6 @@ class SupercarsController < ApplicationController
     @city = City.order('name asc').all
     @country = Country.order('name asc').all
     @supercars = search
-    # @supercars_top_five = top_five_search
     @supercars_top_five = Supercar.last(5).reverse
 
     # @partial = params[:view] || "default", "insta_bird"
@@ -26,21 +25,36 @@ class SupercarsController < ApplicationController
     @make = Make.order('name asc').all
     @car_model = CarModel.order('name asc').all
     @city = City.order('name asc').all
-    @country = Country.order('name asc').all
-
-    
+    @country = Country.order('name asc').all 
 
     # @supercar.spot_type = SpotType.new
+  end
+
+  def create
+    @supercar = Supercar.new(supercar_params)
+    @supercar.make = Make.where(:name => params[:make][:name]).first_or_create
+    @supercar.car_model = CarModel.where(:name => params[:car_model][:name]).first_or_create
+    @supercar.city = City.where(:name => params[:city][:name]).first_or_create
+    @supercar.country = Country.where(:name => params[:country][:name]).first_or_create
+    # @supercar.spot_type = SpotType.new(spot_type_params)
+
+    if current_user
+      @supercar.user = current_user
+    else
+    end
+
+      if @supercar.save
+        redirect_to supercars_url
+      else
+        render :new
+      end
+
   end
 
   def show
     @supercar = Supercar.find(params[:id])
     impressionist(@supercar, "wtf...")
   end
-
-  # def search
-  #   @makes = Make.search params[:search]
-  # end
 
 
 
@@ -58,13 +72,6 @@ class SupercarsController < ApplicationController
 
   def update
     @supercar = Supercar.find(params[:id])
-
-    # @supercar.update(supercar_params)
-    # @supercar.make.update(make_params)
-    # @supercar.car_model.update(car_model_params)
-    # @supercar.city.update(city_params)
-    # @supercar.country.update(country_params)
-    # @supercar.spot_type.update(spot_type_params)
 
     old_make_id = @supercar.make_id
 
@@ -123,26 +130,7 @@ class SupercarsController < ApplicationController
   end
 
 
-  def create
-    @supercar = Supercar.new(supercar_params)
-    @supercar.make = Make.where(:name => params[:make][:name]).first_or_create
-    @supercar.car_model = CarModel.where(:name => params[:car_model][:name]).first_or_create
-    @supercar.city = City.where(:name => params[:city][:name]).first_or_create
-    @supercar.country = Country.where(:name => params[:country][:name]).first_or_create
-    # @supercar.spot_type = SpotType.new(spot_type_params)
-
-    if current_user
-      @supercar.user = current_user
-    else
-    end
-
-      if @supercar.save
-        redirect_to supercars_url
-      else
-        render :new
-      end
-
-  end
+  
   
   def destroy
   end
@@ -202,99 +190,6 @@ private
 
     cars.paginate(:page => params[:page], :per_page => 16).order("created_at DESC")
 
-
-    # search_params
-    # sanitized_search = search_params.delete_if { |k,v| v.blank? }
-    # # query = Card.all
-    # supercar_query = Supercar.all
-    # make_query = Make.all
-    # model_query = CarModel.all
-    # city_query = City.all
-    # country_query = Country.all
-    # sanitized_search.each do |key, value|
-
-     #  if key == "make"
-     #    if Make.where(["name iLIKE ?", "%#{value}%"]).first == nil
-     #      make_query = []
-     #      # query = [query, subtype_query].inject(&:&)      
-     #      query = query.merge(make_query)
-     #    else
-     #      make_query = Make.where(["name iLIKE ?", "%#{value}%"]).first.supercars
-     #      # query = [query, card_type_query].inject(&:&)
-     #      query = query.merge(make_query)
-     #    end
-     #  end
-     #  if key == "car_model"
-     #    if CarModel.where(["name iLIKE ?", "%#{value}%"]).first == nil
-     #      car_model_query = []
-     #      # query = [query, subtype_query].inject(&:&)      
-     #      query = query.merge(car_model_query)
-     #    else
-     #      car_model_query = CarModel.where(["name iLIKE ?", "%#{value}%"]).first.supercars
-     #      # query = [query, card_type_query].inject(&:&)
-     #      query = query.merge(car_model_query)
-     #    end
-     #  end
-     #  if key == "city"
-     #    if City.where(["name iLIKE ?", "%#{value}%"]).first == nil
-     #      city_query = []
-     #      # query = [query, subtype_query].inject(&:&)      
-     #      query = query.merge(city_query)
-     #    else
-     #      city_query = City.where(["name iLIKE ?", "%#{value}%"]).first.supercars
-     #      # query = [query, card_type_query].inject(&:&)
-     #      query = query.merge(city_query)
-     #    end
-     #  end
-     #  if key == "country"
-     #    if Country.where(["name iLIKE ?", "%#{value}%"]).first == nil
-     #      country_query = []
-     #      # query = [query, subtype_query].inject(&:&)      
-     #      query = query.merge(country_query)
-     #    else
-     #      country_query = Country.where(["name iLIKE ?", "%#{value}%"]).first.supercars
-     #      # query = [query, card_type_query].inject(&:&)
-     #      query = query.merge(country_query)
-     #    end
-     #  end    
-    # end
-    # convert query back into Active Record object 
-    # if query.class == Array    
-    #   query = Card.where(id: query.map(&:id))
-    # end
-
-    # query.page(params[:page])
   end
-
-  def top_five_search
-
-
-    fields = {
-      :make_id => Make,
-      :car_model_id => CarModel,
-      :city_id => City,
-      :country_id => Country
-    }
-   
-    cars = Supercar.last(5).reverse
-    # cars = Supercar.all
-
-    fields.each do |field, model|
-      # if query = search_params[field]
-      unless params[field].blank?
-        # subresults = model.where("name like ?", "%#{query}%")
-        subresults = model.find(params[field])
-        cars = cars.where(field => subresults)
-        # binding.pry
-      end
-    end
-
-    # cars.page(params[:page])
-
-    cars.paginate(:page => params[:page], :per_page => 5).order("created_at DESC")
-
-
-  end
-
 end
 
